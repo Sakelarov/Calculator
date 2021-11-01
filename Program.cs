75cbbd1e8e116f44520b1cc32dc568726069c05e
Lyubomir Sakelarov <77864127+Sakelarov@users.noreply.github.com>
2021-11-01 18:03:53 +0200

    Initial commit (#1)
    Calculate basic mathematical operations between 2 numbers
    
    Co-authored-by: Lyubomir Sakelarov <lyubomir.sakelarov@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isTerminated = false;
+
+            while (!isTerminated)
+            {
+                Console.WriteLine("Input the first number:");
+                double firstNum = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Input the operation type:");
+                string operation = Console.ReadLine();
+
+                Console.WriteLine("Input the second number:");
+                double secondNum = double.Parse(Console.ReadLine());
+
+
+                if (operation == "+")
+                {
+                    Console.WriteLine("Result: " + (firstNum + secondNum));
+                    isTerminated = true;
+                }
+                else if (operation == "-")
+                {
+                    Console.WriteLine("Result: " + (firstNum - secondNum));
+                    isTerminated = true;
+                }
+                else if (operation == "*")
+                {
+                    Console.WriteLine("Result: " + firstNum * secondNum);
+                    isTerminated = true;
+                }
+                else if (operation == "/")
+                {
+                    Console.WriteLine("Result: " + firstNum / secondNum);
+                    isTerminated = true;
+                }
+                else
+                {
+                    Console.WriteLine("Unsupported operation type. Try again:");
+                }
+            }
+        }
+    }
+}
